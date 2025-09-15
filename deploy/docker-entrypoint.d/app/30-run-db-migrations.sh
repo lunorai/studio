a1de4d26e73ab49c8a2cba8cc6aca6e4be0e74dc
Prince <princer6450@gmail.com>
2025-09-15 17:36:58 +0530

    Docker issue fix

--- deploy/docker-entrypoint.d/app/30-run-db-migrations.sh
+++ deploy/docker-entrypoint.d/app/30-run-db-migrations.sh
@@ -1,1 +1,12 @@
-../common/30-run-db-migrations.sh
+# ../common/30-run-db-migrations.sh
+
+#!/bin/sh
+set -e ${DEBUG:+-x}
+
+if [ -n "${POSTGRE_HOST:-}" ] || [ -n "${MYSQL_HOST:-}" ] && [ "${SKIP_DB_MIGRATIONS:-}" != "true" ]; then
+  echo >&3 "=> Do database migrations..."
+  python3 /label-studio/label_studio/manage.py locked_migrate >&3
+  echo >&3 "=> Migrations completed."
+else
+  echo >&3 "=> Skipping run db migrations."
+fi
